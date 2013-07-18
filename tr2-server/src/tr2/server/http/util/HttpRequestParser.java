725ae317fb56dbff8788c9b7a01030888505ecb1
Luis Pacheco <luisbelem@gmail.com>
2013-07-18 04:50:55 -0300

    asdasd

--- tr2-server/src/tr2/server/http/util/HttpRequestParser.java
+++ tr2-server/src/tr2/server/http/util/HttpRequestParser.java
@@ -2,7 +2,6 @@ package tr2.server.http.util;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 
 import tr2.server.common.entity.Interval;
